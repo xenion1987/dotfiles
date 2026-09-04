59c6a39a4e844ea06d537bd303dfa9a2b8d71aa2
Xenion1987 <39803750+Xenion1987@users.noreply.github.com>
2026-09-04 20:00:50 +0200

    update: wezterm: add selction colors

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,6 +5,10 @@ local config = wezterm.config_builder()
 -- Fonts & Look
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.85
+config.colors = {
+	selection_bg = "#ffcc66",
+	selection_fg = "#1e1e1e",
+}
 config.color_scheme = "DimmedMonokai"
 config.font = wezterm.font("MesloLGS NF")
 config.font_size = 14
